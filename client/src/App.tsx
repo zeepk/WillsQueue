9ece2b26ee80441fe44c436f280a1bae7fea4e1e
Matt Hughes <mhughes.ipod@gmail.com>
2022-06-25 10:37:21 -0700

    ga

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,10 @@ import CustomNavbar from './Common/CustomNavbar';
 import Dashboard from './Common/Dashboard/Dashboard';
 import LoadingScreen from './Common/LoadingScreen';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize('G-64D995T32R');
+ReactGA.send('pageview');
 
 function App() {
     const { isLoading } = useAuth0();
